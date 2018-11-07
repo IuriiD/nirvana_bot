048642fbdda8a9dee5d59ab3c65c8abe15d86d91
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-11-07 09:35:02 +0200

    +2 stickers; sending images

--- helpers/sendAnswer.js
+++ helpers/sendAnswer.js
@@ -6,7 +6,18 @@ const phrases = require('./phrases');
 function sendAnswer(session, answer) {
   const { text, sticker } = parseAnswer(phrases, answer);
   if (text) session.send(text);
-  if (sticker) session.send(sticker);
+  if (sticker) {
+    session.send({
+      text: sticker,
+      attachments: [
+        {
+          contentType: 'image/png',
+          contentUrl: 'https://iuriid.github.io/public/img/moc_logo.png',
+          name: '1.png',
+        },
+      ],
+    });
+  }
 }
 
 module.exports = { sendAnswer };
