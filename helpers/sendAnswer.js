1b230d7c42419bea860c5e9a14ec5fd05933eef3
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-11-09 17:32:51 +0200

    Sync

--- helpers/sendAnswer.js
+++ helpers/sendAnswer.js
@@ -1,5 +1,5 @@
 // Sends text and stickers. DRY
-
+const builder = require('botbuilder');
 const { parseAnswer } = require('./parseAnswer');
 const phrases = require('./phrases');
 
@@ -7,6 +7,22 @@ function sendAnswer(session, answer) {
   const { text, sticker } = parseAnswer(phrases, answer);
   if (text) session.send(text);
   if (sticker) {
+    const card = new builder.Message(session);
+    card.attachments([
+      new builder.HeroCard(session)
+        .title('Classic White T-Shirt')
+        .subtitle('100% Soft and Luxurious Cotton')
+        .text('Price is $25 and carried in sizes (S, M, L, and XL)')
+        .images([
+          builder.CardImage.create(
+            session,
+            `https://raw.githubusercontent.com/IuriiD/nirvana_bot/master/stickers/${sticker}.png`,
+          ),
+        ])
+        .buttons([builder.CardAction.imBack(session, 'buy classic white t-shirt', 'Buy')]),
+    ]);
+    session.send(card);
+    /*
     session.send({
       text: '',
       attachments: [
@@ -16,7 +32,7 @@ function sendAnswer(session, answer) {
           name: `${sticker}.png`,
         },
       ],
-    });
+    }); */
   }
 }
 
