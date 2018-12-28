fe6034b5485e8d48603847f3b4090386020dbf19
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-12-28 15:00:31 +0200

    Added typing - it was easy

--- dialogs/main-flow.js
+++ dialogs/main-flow.js
@@ -16,28 +16,34 @@ async function mainFlow(session, recognizer) {
       // 1.1 User clicked 'Play' button
       if (text.includes('[### play ###]')) {
         const playId = text.split('[### play ###]')[1];
+        session.sendTyping();
         replies.sendAudio(session, playId, stickersObj);
 
         // 1.2 User clicked "Show more" [plays] button
       } else if (text.includes('[### next ###]')) {
         const nextPlaysIds = text.split('[### next ###]')[1].split('|');
+        session.sendTyping();
         replies.presentPlays(session, nextPlaysIds, stickersObj, true);
 
         // 2. Process callbacks/payloads from Commands Menu (Telegram) etc
         // 2.1 Telegram - /random (Telegram) or [### payload ###]random_phrase (Facebook)
       } else if (text === '/random' || text === '[### payload ###]random_phrase') {
+        session.sendTyping();
         replies.randomPhrase(session, stickersObj);
 
         // 2.2 Telegram - /faq (Telegram) or [### payload ###]faq (Facebook)
       } else if (text === '/faq' || text === '[### payload ###]faq') {
+        session.sendTyping();
         await replies.getFaq(session, stickersObj);
 
         // 2.3 Telegram - /contacts (Telegram) or [### payload ###]feedback (Facebook)
       } else if (text === '/contacts' || text === '[### payload ###]feedback') {
+        session.sendTyping();
         replies.feedback(session);
 
         // 3. Process clicks on /start (Telegram) or Getting started (FBM) buttons
       } else if (text === '/start' || text === '[### payload ###]facebook_welcome') {
+        session.sendTyping();
         await replies.getFaq(session, stickersObj);
 
         // 4. Process text inputs
@@ -45,6 +51,7 @@ async function mainFlow(session, recognizer) {
         recognizer.recognize(session, async (err, data) => {
           if (err) {
             console.log(`Error: ${err}`);
+            session.sendTyping();
             replies.sendAnswer(session, i18n.__('error_happened'), stickersObj);
           }
 
@@ -52,24 +59,26 @@ async function mainFlow(session, recognizer) {
             // Try to search users's input in texts
             const relevantPlaysIds = search(data.utterance, texts);
             if (relevantPlaysIds) {
+              session.sendTyping();
               replies.presentPlays(session, relevantPlaysIds, stickersObj);
             } else {
               // If nothing found - Default fallback answer
+              session.sendTyping();
               replies.sendAnswer(session, i18n.__('dont_understand'), stickersObj);
             }
           } else {
-            console.log('\n#HERE');
+            session.sendTyping();
             replies.sendAnswer(session, data.answer, stickersObj);
           }
         });
       }
     } else if (session.message.attachments && session.message.attachments.length > 0) {
+      session.sendTyping();
       replies.sendAnswer(session, i18n.__('dont_understand'), stickersObj);
     }
     return;
   } catch (error) {
     console.log(`\n⚠ tStickerWButtons():\n${error}`);
-    return false;
   }
 }
 
